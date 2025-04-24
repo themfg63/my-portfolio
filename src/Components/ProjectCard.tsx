abb24ce15add47b55893d799f445d48998ef7849
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2025-04-24 14:03:00 +0300

    Project Module updated

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,7 +1,11 @@
 import { Badge, Button, Card, Group, Image, Indicator, Text } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks";
+import FullProjectModal from "./FullProjectModal";
 
 const ProjectCard = (props:any) => {
-    return <Card className="!bg-bgColor cursor-pointer transition-transform duration-300 ease-in-out hover:!scale-[1.02] mb-5 hover:!shadow-[0_0_10px_1px_#64FFDA] !border-primaryColor border-2" w="360px" shadow="lg" padding="sm" radius="lg" withBorder >
+    const [opened,{open,close}] = useDisclosure(false);
+
+    return<> <Card onClick={open} className="!bg-bgColor cursor-pointer transition-transform duration-300 ease-in-out hover:!scale-[1.02] mb-5 hover:!shadow-[0_0_10px_1px_#64FFDA] !border-primaryColor border-2" w="360px" shadow="lg" padding="sm" radius="lg" withBorder >
         <Card.Section className="p-3">
             <Image className="!rounded-xl !shadow-[0_0_5px_#64FFDA]" src={props.image} alt={props.image} />
         </Card.Section>
@@ -9,7 +13,7 @@ const ProjectCard = (props:any) => {
         <Group justify="space-between" mt="xs" mb="xs">
             <Text className="!text-2xl gap-2 !font-bold !text-white flex items-center">
                 {props.title}
-                {props.live===true && <Badge variant="outline" color="red" rightSection={<Indicator color="red" position="middle-end" size={7} processing></Indicator>}>Canlı</Badge>}
+                {props.live===true && <Badge variant="outline" color="red" rightSection={<Indicator color="red" position="middle-end" size={7} processing></Indicator>}>Full Stack</Badge>}
             </Text>
         </Group>
         <Group mb="xs">
@@ -18,10 +22,12 @@ const ProjectCard = (props:any) => {
         <Text className="!text-justify" lineClamp={5} size="sm" c="dimmed">
             {props.desc}
         </Text>
-        <Button className="!bg-primaryColor !text-bgColor" fullWidth mt="md" radius="md">
+        <Button onClick={open} className="!bg-primaryColor !text-bgColor" fullWidth mt="md" radius="md">
             Detaylı İncele
         </Button>
     </Card>
+    <FullProjectModal opened={opened} close={close} title={props.title} desc={props.desc} image={props.image} live={props.live} link={props.link} github={props.github} technologies={props.technologies}/>
+    </>
 }
 
 export default ProjectCard;
--- src/User.tsx
+++ src/User.tsx
@@ -9,7 +9,7 @@ const ProjectInfo=[
         title: "E-Commerce App",
         desc: "Hem kullanıcı hem de admin tarafında güçlü özellikler sunan E-Ticaret platformumu sizlerle paylaşmaktan heyecan duyuyorum. Bu Projede backend tarafında Spring Boot ve frontend tarafında Spring Boot'un bir kütüphanesi olan Thymeleaf ile HTML,CSS,JavaScript kullanarak modern ve güvenli bir alışveriş platformu oluşturdum.",
         image: "springboot.png",
-        live: false,
+        live: true,
         technologies:["Spring Boot","Thymeleaf","HTML","CSS","JavaScript","Bootstrap","MySQL"],
         link:"https://github.com/themfg63/E-Commerce-FULL-STACK-",
         github:"https://github.com/themfg63/E-Commerce-FULL-STACK-"
@@ -18,7 +18,7 @@ const ProjectInfo=[
         title:"E-Learn",
         desc:"Lisans eğitimim son eğitim döneminde yaptığım bitirme projemdir. Spring Boot ve Thymeleaf kullanarak oluşturduğum full stack projenin online eğitim platformlarından farksız bir şekilde tasarlanması, eğitimlerin alınması, eğitmenlerle iletişime geçilmesi imkanını sağlıyor. Kullanıcı dostu bir arayüzle kullanıma sunulacak hale getirdiğim bu projede veritabanında MySQL kullandım.",
         image: "springboot.png",
-        live: false,
+        live: true,
         technologies:["Spring Boot","Thymeleaf","HTML","CSS","JavaScript","Bootstrap","MySQL"],
         link: "https://github.com/themfg63/E-Learn",
         github: "https://github.com/themfg63/E-Learn"
@@ -27,7 +27,7 @@ const ProjectInfo=[
         title: "Hotel Management",
         desc: "Java ve React kullanarak geliştirdiğim bir Full Stack proje. Otel rezervasyonu, oda bilgileri, fiyat bilgileri ve müsaitlik durumunu kolaylıkla kontrol edebileceğin bir rezervasyon uygulaması. Backendinde Spring Boot, frontendinde React ve veritabanında PostgreSQL kullanarak geliştirdiğim bir projedir.",
         image: "springboot.png",
-        live: false,
+        live: true,
         technologies:["Spring Boot","React","Bootstrap","JavaScript","Heroicons","PostgreSQL"],
         link: "https://github.com/themfg63/HotelManagement-FULL-STACK-Backend",
         github: "https://github.com/themfg63/HotelManagement-FULL-STACK-Backend"
@@ -36,6 +36,7 @@ const ProjectInfo=[
         title: "Job Find",
         desc: "Sadece Yazılım dünyasına ait bir iş bulma uygulaması. Full stack olarak geliştirdiğim projenin backend'inde Spring Boot, frontendinde React kullandım. Typescript ve Tailwindcss kullanarak kulllanıcı dostu ve kolay anlaşılır bir arayüz tasarladım. Arka tarafında iş başvurusu, ilan paylaşımı gibi işlevleri RESTAPI'lerle gerçekleştirdim. Projenin veritabanında MySQL kullandım.",
         image: "springboot.png",
+        live:true,
         technologies:["Spring Boot","React","Typescript","TailwindCSS","Heroicons","MySQL"],
         link: "https://github.com/themfg63/JobFind",
         github: "https://github.com/themfg63/JobFind"
