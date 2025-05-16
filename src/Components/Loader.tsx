bed55f23a1733352cfb83261068a1e25058288fe
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2025-05-16 14:11:17 +0300

    starter

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -4,7 +4,7 @@ import IconCloud from "./magicui/icon-cloud";
 
 export function Loader() {
     return(
-        <div className="relative flex h-full w-full animate-[ping_1.5s_ease-in-out_1_4.5s] items-center justify-center px-20 pb-20 pt-8">
+        <div className="relative flex h-full w-full animate-[ping_1.5s_ease-in-out_1_4.2s] items-center justify-center px-20 pb-20 pt-8">
             <IconCloud iconSlugs={Slugs} />
             <IconHexagon className="absolute -z-10 animate-[spin_5s_linear_infinite]" size={150} color="#64FFDA" stroke={1.25} />
             <div className="absolute font-mono text-primaryColor font-semibold text-6xl -z-10">MFG</div>
